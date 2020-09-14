fe7b22bc3f9a2cd7ace297694c9f7703004fc4fd
adamlipski <adamlipski@o2.pl>
2020-09-14 18:11:57 +0200

    Drawing pendulum and depend x and y

--- PendulumSimGraph/MainWindow.xaml.cs
+++ PendulumSimGraph/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -27,56 +28,73 @@ namespace PendulumSimGraph
         double y_p = 40;
         double[] points = { 10, 20 };
 
-    
-        public class MyDouble : INotifyPropertyChanged
-        {
-            public event PropertyChangedEventHandler PropertyChanged;
 
-            private double _Value;
-            public double Value
-            {
-                get { return _Value; }
-                set { _Value = value; OnPropertyChanged("Value"); }
-            }
 
-            void OnPropertyChanged(string propertyName)
-            {
-                var handler = PropertyChanged;
-                if (handler != null)
-                {
-                    handler(this, new PropertyChangedEventArgs(propertyName));
-                }
-            }
 
-        }
-        ObservableCollection<MyDouble> Arr { get; set; }
+        ObservableCollection<string> point = new ObservableCollection<string>();
         public MainWindow()
         {
             InitializeComponent();
-            Arr.Add(x_p);
-            point.Add(y_p);
+            var stringTemp = points[0].ToString();
+            point.Add(stringTemp);
+            point.Add(points[1].ToString());
             rysownik.DataContext = point;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            PendulumSim pendulum = new PendulumSim(
-                Convert.ToDouble(txtbOmega.Text),
-                Convert.ToDouble(txtbTeta.Text),
-                Convert.ToDouble(txtbdt.Text),
-                Convert.ToDouble(txtbRadius.Text));
-                
-                
-            x_p = Convert.ToDouble(Xp.Text.ToString());
-            y_p = Convert.ToDouble(Yp.Text.ToString());
+            //PendulumSim pendulum = new PendulumSim(
+            //    Convert.ToDouble(txtbOmega.Text),
+            //    Convert.ToDouble(txtbTeta.Text),
+            //    Convert.ToDouble(txtbdt.Text),
+            //    Convert.ToDouble(txtbRadius.Text));
 
-            point[0] = x_p;
-            point[1] = y_p;
-            
-            
+            double omega, teta, t, dt, x, y, r, g;
+
+
+            r = 40;       //[m]
+            g = 9.8f;    //[m/s^2]
+            dt = 0.01f;
+
+            omega = 0.0f;
+            int tetaDeg = 45; // degrees
+
+            //teta = 4.0f;
+
+
+            //Converting degrees to radians
+
+            //teta2 = (float)((Math.PI / 180) * (float)tetaDeg);
+            teta = Math.PI / 180 * tetaDeg;
+
+            //Console.WriteLine($"Converstion from {tetaDeg} degrees to {teta2} radians");
+            //Console.WriteLine("Sin of 30 deg:" + Math.Sin(teta2));
+
+            for (t = 0; t < 10; t += dt)
+            {
+             
+
+                omega = (float)(omega + (g / r) * Math.Sin(teta) * dt);
+
+                teta = teta - omega * dt;
+
+
+
+                double angle = (teta * 180) / Math.PI;
+
+                x = (float)(r * Math.Sin(teta));
+                y = (float)(r * Math.Cos(teta));
+                Thread.Sleep(1);
+                wahadlo.X1 = x;
+                Thread.Sleep(1);
+                wahadlo.Y1 = y;
+                Thread.Sleep(1);
+
+
+
+            }    
+
         }
-
-
 
     }
 }
