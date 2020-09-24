9671f8089672b677b8f81f23e0e9636e9e19a640
adamlipski <adamlipski@o2.pl>
2020-09-24 20:59:36 +0200

    Drawing simple graph

--- PendulumSimGraph/obj/Release/netcoreapp3.1/UserControls/PointLineGraph.g.cs
+++ PendulumSimGraph/obj/Release/netcoreapp3.1/UserControls/PointLineGraph.g.cs
@@ -54,7 +54,7 @@ namespace PendulumSimGraph.UserControls {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PendulumSimGraph;V1.0.0.0;component/usercontrols/pointlinegraph.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PendulumSimGraph;component/usercontrols/pointlinegraph.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\UserControls\PointLineGraph.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
